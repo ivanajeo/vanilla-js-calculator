fbc07625026032adcef565f70b013093fd3f7b27
Van <152158486+ivanajeo@users.noreply.github.com>
2024-08-14 15:39:46 +0700

    Link every button in js

---
+++ script.js
@@ -0,0 +1,25 @@
+const upperDis = document.querySelector(".upperDis");
+const bottomDis = document.querySelector("bottomDis");
+
+const clearBtn = document.querySelector(".clearBtn");
+const deleteBtn = document.querySelector(".deleteBtn");
+const nineBtn = document.querySelector(".nineBtn");
+const eightBtn = document.querySelector(".eightBtn");
+const sevenBtn = document.querySelector(".sevenBtn");
+const sixBtn = document.querySelector(".sixBtn");
+const fiveBtn = document.querySelector(".fiveBtn");
+const fourBtn = document.querySelector(".fourBtn");
+const threeBtn = document.querySelector(".threeBtn");
+const twoBtn = document.querySelector(".twoBtn");
+const oneBtn = document.querySelector(".oneBtn");
+const plusMinusBtn = document.querySelector(".plusMinusBtn");
+const zeroBtn = document.querySelector(".zeroBtn");
+const dotBtn = document.querySelector(".dotBtn");
+
+const rootBtn = document.querySelector(".rootBtn");
+const divisionBtn = document.querySelector(".divisionBtn");
+const percentBtn = document.querySelector(".percentBtn");
+const multiplicationBtn = document.querySelector(".multiplicationBtn");
+const equalBtn = document.querySelector(".equalBtn");
+const subtractionBtn = document.querySelector(".subtractionBtn");
+const additionBtn = document.querySelector(".additionBtn");
