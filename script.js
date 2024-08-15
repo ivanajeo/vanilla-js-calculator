4125f14d79558d08d300dc2a737854d048289032
Van <152158486+ivanajeo@users.noreply.github.com>
2024-08-15 18:13:04 +0700

    Add variables to hold nums and operator, and the event listenet

--- script.js
+++ script.js
@@ -24,6 +24,13 @@ const equalBtn = document.querySelector(".equalBtn");
 const subtractionBtn = document.querySelector(".subtractionBtn");
 const additionBtn = document.querySelector(".additionBtn");
 
+const numberBtn = document.querySelectorAll(".numberBtn");
+const operatorBtn = document.querySelectorAll(".operatorBtn");
+
+let firstNumber = null;
+let secondNumber = null;
+let selectedOperator = null;
+
 const add = function(num1, num2) {
     return num1 + num2;
 }
@@ -47,3 +54,39 @@ const root = function(num1) {
 const percent = function(num1) {
     return num1 / 100;
 }
+
+numberBtn.forEach((button) => {
+    button.addEventListener("click", function () {
+        if (firstNumber === null) {
+            firstNumber = parseInt(this.getAttribute('data-num'));
+            console.log('First Number:', firstNumber);
+        } else if (selectedOperator !== null) {
+            secondNumber = parseInt(this.textContent);
+            console.log('Second Number:', secondNumber);
+        }
+    });
+});
+
+operatorBtn.forEach((button) => {
+    button.addEventListener("click", function() {
+        selectedOperator = this.getAttribute('data-operator');
+        console.log('Selected Operator:', selectedOperator);
+});
+})
+
+
+equalBtn.addEventListener("click", function() {
+    if (firstNumber !== null && secondNumber !== null) {
+        if (selectedOperator === "add") {
+            console.log(add(firstNumber, secondNumber));
+        } else if (selectedOperator === "subtract") {
+            console.log(subtract(firstNumber, secondNumber));
+        } else if (selectedOperator === "multiply") {
+            console.log(multiply(firstNumber, secondNumber));
+        } else if (selectedOperator === "divide") {
+            console.log(divide(firstNumber, secondNumber));
+        }
+
+    }
+});
+
