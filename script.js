47ee72f192de21a8d6afc671eee3327a234c0a9c
Van <152158486+ivanajeo@users.noreply.github.com>
2024-08-18 09:33:24 +0700

    Logic is done

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const upperDis = document.querySelector(".upperDis");
 const bottomDis = document.querySelector(".bottomDis");
 
+const equalBtn = document.querySelector(".equalBtn");
 const numberBtns = document.querySelectorAll(".numberBtn");
 const operatorBtns = document.querySelectorAll(".operatorBtn");
 
-bottomDis.textContent = 0;
+bottomDis.textContent = "0";
 let firstNumber = "";
 let secondNumber = "";
 let selectedOperator = null;
@@ -19,7 +20,19 @@ const operations = {
     percent: (num1) => num1 / 100
 };
 
+function doOperation(firstNum, secondNum, selectedOperation) {
+    result = operations[selectedOperation](parseFloat(firstNum), parseFloat(secondNum));
+    firstNumber = result;
+    selectedOperator = null;
+    secondNumber = "";   
+    console.log("firstNumber(new) ", firstNumber);
+    console.log("secondNumber(prenew): ", secondNumber);
+    console.log("selectedOperator(new) ", selectedOperator);
+    console.log("result(new) ", result);
+    
 
+    return result;
+}
 function updateDisplay(content) {
     bottomDis.textContent += content;
 }
@@ -32,8 +45,87 @@ function resetDisplay() {
 numberBtns.forEach(function(numberBtn) {
     numberBtn.addEventListener("click", function() {
         const digit = this.getAttribute("data-num");
-        firstNumber += digit;
-        console.log(digit);
-        console.log(firstNumber);
+        if (result === null) {
+            if (bottomDis.textContent === "0") {
+                resetDisplay();
+            }
+            if (selectedOperator === null) {
+                firstNumber += digit;
+                updateDisplay(digit);
+                console.log("digit: ", digit);
+                console.log("firstNumber: ", firstNumber);
+            } else {
+                secondNumber += digit;
+                updateDisplay(digit);
+                console.log("digit: ", digit);
+                console.log("secondNumber: ", secondNumber);
+            }
+        } else {
+            if (selectedOperator === null) {
+                resetDisplay();
+                updateDisplay(firstNumber);
+                firstNumber += digit;
+                updateDisplay(digit);          
+
+                              
+                console.log("digit(new): ", digit);
+                console.log("firstNumber(new): ", firstNumber);
+                
+
+            } else {
+                secondNumber += digit;
+                updateDisplay(digit);
+                console.log("digit: ", digit);
+                console.log("secondNumber(new): ", secondNumber);
+                result = null;
+                console.log(result);
+            }
+
+        }
+
     })
+})
+
+operatorBtns.forEach(function(operatorBtn) {
+    operatorBtn.addEventListener("click", function() {
+        if (selectedOperator !== null && result === null 
+            && firstNumber !== "" && secondNumber !== "") {
+            
+
+            console.log("1", selectedOperator);
+
+            doOperation(firstNumber, secondNumber, selectedOperator);
+            console.log("2", selectedOperator);
+
+            resetDisplay();
+            updateDisplay(firstNumber)
+
+            selectedOperator = this.getAttribute("data-operator");
+            updateDisplay(` ${operatorBtn.textContent} `);
+
+
+            console.log("3", selectedOperator);
+        } else if (firstNumber !== "") {
+            resetDisplay();
+            updateDisplay(firstNumber);
+
+            selectedOperator = this.getAttribute("data-operator");
+            updateDisplay(` ${operatorBtn.textContent} `);
+            
+           
+            console.log("selectedOperator ", selectedOperator);
+        }
+    })
+})
+
+equalBtn.addEventListener("click", function() {
+    doOperation(firstNumber, secondNumber, selectedOperator);
+    updateDisplay(` = ${result}`);
+
+    // firstNumber = result;
+    // result = null;
+    // selectedOperator = null;
+    // console.log("firstNumber(new) ", firstNumber);
+    // console.log("result(new) ", result);
+    // console.log("selectedOperator(new) ", selectedOperator);
 })
